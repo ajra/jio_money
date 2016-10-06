834df0f4cf6a45dba6dc543424c0ef88c7460c98
sthiritha <sthiritha.velu@kaaylabs.com>
2016-10-06 14:04:48 +0530

    order page functionality added

--- app/src/main/java/com/jiomoney/snapbizz/adapter/TodayOrderAdapter.java
+++ app/src/main/java/com/jiomoney/snapbizz/adapter/TodayOrderAdapter.java
@@ -1,5 +1,6 @@
 package com.jiomoney.snapbizz.adapter;
 
+import android.app.Activity;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -10,33 +11,84 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jiomoney.snapbizz.R;
+import com.jiomoney.snapbizz.ui.orders.model.TodayOrder;
 
-/**
- * Created by sthiritha on 5/10/16.
- */
+import java.util.List;
+
 public class TodayOrderAdapter extends RecyclerView.Adapter<TodayOrderAdapter.TodayViewHolder> {
+
+    TodayOrder todayOrder = new TodayOrder();
+    List<TodayOrder.OrderList> todayOrdersInfo;
+    Activity context;
+    TodayViewHolder tvh;
+
+    public TodayOrderAdapter(List<TodayOrder.OrderList> todayOrderList, Activity context) {
+        this.todayOrdersInfo = todayOrderList;
+        this.context = context;
+    }
+
     @Override
     public TodayOrderAdapter.TodayViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_today_transaction, parent, false);
-        return null;
+        tvh = new TodayViewHolder(v);
+        return tvh;
     }
     @Override
     public void onBindViewHolder(TodayOrderAdapter.TodayViewHolder holder, int position) {
+//        TodayOrder.OrderList t_info=todayOrdersInfo.get(position);
+        holder.myOrderName.setText(todayOrdersInfo.get(position).customerName);
+        holder.orderDate_time.setText((todayOrdersInfo.get(position).orderDate));
+        String status_new =todayOrdersInfo.get(position).orderStatusDescription;
+
+        if("New".equals(status_new) && !status_new.equals(null)){
+            holder.newImageView.setVisibility(View.VISIBLE);
+            holder.inProcessImageView.setVisibility(View.GONE);
+            holder.deliveryImageView.setVisibility(View.GONE);
+            holder.cancelImageView.setVisibility(View.GONE);
+        }else if("In process".equals(status_new) && !status_new.equals(null)){
+            holder.newImageView.setVisibility(View.GONE);
+            holder.inProcessImageView.setVisibility(View.VISIBLE);
+            holder.deliveryImageView.setVisibility(View.GONE);
+            holder.cancelImageView.setVisibility(View.GONE);
+        }else if("Delivery".equals(status_new) && !status_new.equals(null)){
+            holder.newImageView.setVisibility(View.GONE);
+            holder.inProcessImageView.setVisibility(View.GONE);
+            holder.deliveryImageView.setVisibility(View.VISIBLE);
+            holder.cancelImageView.setVisibility(View.GONE);
+        }else if("Canceled".equals(status_new) && !status_new.equals(null)){
+            holder.newImageView.setVisibility(View.GONE);
+            holder.inProcessImageView.setVisibility(View.GONE);
+            holder.deliveryImageView.setVisibility(View.GONE);
+            holder.cancelImageView.setVisibility(View.VISIBLE);
+        }else {
+            holder.newImageView.setVisibility(View.GONE);
+            holder.inProcessImageView.setVisibility(View.GONE);
+            holder.deliveryImageView.setVisibility(View.GONE);
+            holder.cancelImageView.setVisibility(View.GONE);
+        }
     }
     @Override
     public int getItemCount() {
-        return 0;
+        return  todayOrder.orderList.size();
     }
     public static class TodayViewHolder extends RecyclerView.ViewHolder {
         LinearLayout type_of_transaction;
         ImageView newImageView,inProcessImageView,deliveryImageView,cancelImageView;
+        TextView sNo,myOrderName,orderDate_time;
         public TodayViewHolder(View itemView) {
             super(itemView);
             type_of_transaction=(LinearLayout) itemView.findViewById(R.id.linear_Layout_value);
-            newImageView=(ImageView) itemView.findViewById(R.id.name_image_view);
+            newImageView=(ImageView) itemView.findViewById(R.id.new_image_view);
             inProcessImageView=(ImageView) itemView.findViewById(R.id.in_process_image_view);
-            deliveryImageView=(ImageView) itemView.findViewById(R.id.in_process_image_view);
+            deliveryImageView=(ImageView) itemView.findViewById(R.id.delivery_image_view);
             cancelImageView=(ImageView) itemView.findViewById(R.id.canceled_image_view);
-             }
+            sNo=(TextView) itemView.findViewById(R.id.sno_text_view);
+            myOrderName=(TextView) itemView.findViewById(R.id.my_order_text_view);
+            orderDate_time=(TextView) itemView.findViewById(R.id.my_order_date_time_text_view);
+        }
+    }
+    public void clear() {
+        todayOrder.orderList.clear();
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/jiomoney/snapbizz/ui/orders/OrdersActivity.java
+++ app/src/main/java/com/jiomoney/snapbizz/ui/orders/OrdersActivity.java
@@ -1,22 +1,91 @@
 package com.jiomoney.snapbizz.ui.orders;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.jiomoney.snapbizz.R;
+import com.jiomoney.snapbizz.adapter.TodayOrderAdapter;
+import com.jiomoney.snapbizz.ui.orders.model.TodayOrder;
+import com.jiomoney.snapbizz.utils.AppLog;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OrdersActivity extends AppCompatActivity {
-
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.today_Order_RecyclerView)
+    RecyclerView todayOrderRecyclerView;
+    @BindView(R.id.linearLayouttitle)
+    LinearLayout linearLayouttitle;
+    @BindView(R.id.llMainTittle)
+    LinearLayout llMainTittle;
+    @BindView(R.id.content_orders)
+    RelativeLayout contentOrders;
+    List<TodayOrder.OrderList> todayOrderList;
+    Activity context;
+    LinearLayoutManager mLayoutManager;
+    TodayOrder todayOrderPojo;
+    TodayOrderAdapter todayOrderAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orders);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        todayOrderList = new ArrayList<>();
+        todayOrderRecyclerView.setHasFixedSize(true);
+        mLayoutManager=new LinearLayoutManager(this);
+        todayOrderRecyclerView.setLayoutManager(mLayoutManager);
+        todayOrderPojo=new TodayOrder();
+        try {
+            GsonBuilder gsonBUilder = new GsonBuilder();
+            Gson gson = gsonBUilder.create();
+            todayOrderPojo = gson.fromJson(loadJSONFromAsset("today_orders.json"), TodayOrder.class);
+            todayOrderList.clear();
+            for (int i = 0; i < todayOrderPojo.orderList.size(); i++) {
+                todayOrderList.add(todayOrderPojo.orderList.get(i));
+                System.out.println("Today order list: " + todayOrderPojo.orderList.get(i));
+                System.out.println("Today order list: " + todayOrderPojo.orderList);
+                System.out.println("Today order list: " + todayOrderList);
+            }
+        } catch (Exception e) {
+            AppLog.handleException(AppLog.TAG, e);
+        }
+        todayOrderAdapter = new TodayOrderAdapter(todayOrderList,context);
+        todayOrderRecyclerView.setAdapter(todayOrderAdapter);
     }
 
+    public String loadJSONFromAsset(String string) {
+        String json = null;
+        try {
+            InputStream is = OrdersActivity.this.getAssets().open(string);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            AppLog.handleException(AppLog.TAG, ex);
+            return null;
+        }
+        return json;
+    }
 }
